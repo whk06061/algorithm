413e2c5d5f6766242b265d15e4fd15f9a93b4d3f
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-05-01 17:30:46 +0900

    05/01

---
+++ 41. 연속된 자연수의 합.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int  n, i, x=2, y=1, tmp, cnt=0;
+	scanf("%d", &n);
+	while(n-y>0){
+		tmp = n-y;
+		if((tmp)%x==0){
+			for(i=0; i<x-1; i++){
+				printf("%d + ", tmp/x+i);
+			}
+			printf("%d = %d\n", tmp/x+i, n);
+			cnt++;
+		}
+			y = y+x;
+			x++;
+	}
+	printf("%d", cnt);
+} 
+	
+
+
+
+
+
+
+
+
+
+
