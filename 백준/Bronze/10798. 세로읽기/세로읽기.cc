8d310182cd5eadbc8bc54a852924bf7dae3c2e5d
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-07-01 03:09:10 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+#define SIZE 5
+vector<string> v(SIZE);
+
+int main() {
+    // 입력 받기
+    for (int i = 0; i < SIZE; ++i) {
+        string s;
+        cin >> s;
+        v[i] = s;
+    }
+
+    // 출력하기
+    for (int i = 0; i < 15; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            if (v[j].size() > i)
+                cout << v[j][i];
+        }
+    }
+}
