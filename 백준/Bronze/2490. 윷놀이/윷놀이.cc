442fc7de56c55b1c46d2914231eccaf490b6ef05
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-07-01 19:19:27 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,34 +1,12 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
 int main() {
     for (int i = 0; i < 3; ++i) {
-        int x = 0; //배
-        int y = 0; //등
-        for (int j = 0; j < 4; ++j) {
-            int a;
-            cin >> a;
-            if (a == 0) x++;
-            if (a == 1) y++;
-        }
-        switch (x) {
-            case 1:
-                cout << "A" << endl;
-                break;
-            case 2:
-                cout << "B" << endl;
-                break;
-            case 3:
-                cout << "C" << endl;
-                break;
-            case 4:
-                cout << "D" << endl;
-                break;
-            default:
-                cout << "E" << endl;
-                break;
-        }
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int sum = a + b + c + d;
+        cout << "DCBAE"[sum] << endl;
     }
 }
