cd67f7c4c65b8ef4f2e4936b9a1b14895a554b5f
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-08-12 03:33:06 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -5,26 +5,22 @@ using namespace std;
 
 int n, a, b;
 vector<pair<int, int>> v;
-vector<int> c;
 
-int main(){
+int main() {
     cin >> n;
-    c.assign(n, 1);
-    while (n){
+    while (n) {
         cin >> a >> b;
         v.push_back({a, b});
         n--;
     }
-    for(int i = 0; i<v.size(); i++){
+    for (int i = 0; i < v.size(); i++) {
         pair<int, int> cur = v[i];
+        int cnt = 1;
         for (int j = 0; j < v.size(); ++j) {
-            // v[j] 보다 v[i] 가 크면 c[j] += 1
-            if (v[j].first < cur.first && v[j].second < cur.second){
-                c[j] += 1;
+            if (v[j].first > cur.first && v[j].second > cur.second) {
+                cnt++;
             }
         }
-    }
-    for(int x: c){
-        cout << x << " ";
+        cout << cnt << " ";
     }
 }
