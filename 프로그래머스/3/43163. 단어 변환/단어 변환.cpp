00bc36f75ff4e385f5739f1e499f732313512dcd
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-09-02 06:26:14 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<bool> check;
+int answer = 0;
+
+bool wordCheck(string word1, string word2) {
+    bool same = true;
+    for (int i = 0; i < word1.length(); ++i) {
+        if (word1[i] != word2[i]) {
+            if (!same) return false;
+            same = false;
+        }
+    }
+    return true;
+}
+
+void bfs(string begin, string target, vector<string> words){
+    queue<pair<string, int>> q;
+    q.push({begin, 0});
+    while (!q.empty()){
+        pair<string, int> cur = q.front();
+        q.pop();
+        if (cur.first == target) {
+            answer = cur.second;
+            return;
+        }
+        for (int i = 0; i < words.size(); ++i) {
+            string next = words[i];
+            if (!check[i] && wordCheck(cur.first, next)){
+                q.push({next, cur.second + 1});
+                check[i] = true;
+            }
+        }
+    }
+    answer = 0;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    check.assign(words.size(), false);
+    bfs(begin, target, words);
+    return answer;
+}
