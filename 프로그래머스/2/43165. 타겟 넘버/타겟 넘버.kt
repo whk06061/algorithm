1216fd479f410d1f7e70aaed46e174d4fbdf7698
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-10-04 03:49:43 +0900

    [level 2] Title: 타겟 넘버, Time: 239.54 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -4,17 +4,23 @@ import java.util.Queue
 class Solution {
     fun solution(numbers: IntArray, target: Int): Int {
         var answer = 0
+        
         val q: Queue<Pair<Int, Int>> = LinkedList()
-        q.add(Pair(numbers[0], 0))
         q.add(Pair(-numbers[0], 0))
-        while (q.isNotEmpty()) {
-            val (temp, idx) = q.poll()
-            val nextIdx = idx + 1
-            if (nextIdx < numbers.size) {
-                q.add(Pair(temp + numbers[nextIdx], nextIdx))
-                q.add(Pair(temp - numbers[nextIdx], nextIdx))
-            } else if (temp == target) answer++
+        q.add(Pair(numbers[0], 0))
+        
+        while(q.isNotEmpty()){
+            val current = q.poll()
+            val value = current.first
+            val nextIndex = current.second + 1
+            if(nextIndex == numbers.size){
+                if(value == target) answer ++
+            }else{
+                q.add(Pair(value - numbers[nextIndex], nextIndex))
+                q.add(Pair(value + numbers[nextIndex], nextIndex))
+            }
         }
+        
         return answer
     }
 }
