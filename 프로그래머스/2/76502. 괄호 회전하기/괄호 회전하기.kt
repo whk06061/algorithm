377e77a44e81a1e5c8e7f3214b4f814dcea63b35
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2024-10-05 05:57:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 55.59 ms, Memory: 65.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -0,0 +1,34 @@
+class Solution {
+
+    private fun check(s: String): Boolean {
+
+        val temp = mutableListOf<Char>()
+
+        for (c in s) {
+            if (c == '{' || c == '[' || c == '(') {
+                temp.add(c)
+            } else {
+                if (temp.isEmpty()) return false;
+                when (c) {
+                    '}' -> if (temp.last() == '{') temp.removeLast()
+                    ']' -> if (temp.last() == '[') temp.removeLast()
+                    ')' -> if (temp.last() == '(') temp.removeLast()
+                }
+            }
+        }
+
+        return temp.isEmpty()
+    }
+
+    fun solution(s: String): Int {
+        var answer: Int = 0
+        var temp = s
+        repeat(s.length) {
+            val first = temp.first()
+            temp = temp.substring(1).plus(first)
+            if (check(temp)) answer++
+        }
+
+        return answer
+    }
+}
